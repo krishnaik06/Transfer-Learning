24f65febcdf6c038f30ed4d95d1445e7ae48e8f1
Krish C Naik <krishnaik06@gmail.com>
2019-02-21 19:10:04 +0530

    Add files via upload

---
+++ face_Recognition.py
@@ -0,0 +1,108 @@
+
+# -*- coding: utf-8 -*-
+"""
+@author: Krish.Naik
+"""
+
+from keras.layers import Input, Lambda, Dense, Flatten
+from keras.models import Model
+from keras.applications.vgg16 import VGG16
+from keras.applications.vgg16 import preprocess_input
+from keras.preprocessing import image
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+import numpy as np
+from glob import glob
+import matplotlib.pyplot as plt
+
+# re-size all the images to this
+IMAGE_SIZE = [224, 224]
+
+train_path = 'Datasets/Train'
+valid_path = 'Datasets/Test'
+
+# add preprocessing layer to the front of VGG
+vgg = VGG16(input_shape=IMAGE_SIZE + [3], weights='imagenet', include_top=False)
+
+# don't train existing weights
+for layer in vgg.layers:
+  layer.trainable = False
+  
+
+  
+  # useful for getting number of classes
+folders = glob('Datasets/Train/*')
+  
+
+# our layers - you can add more if you want
+x = Flatten()(vgg.output)
+# x = Dense(1000, activation='relu')(x)
+prediction = Dense(len(folders), activation='softmax')(x)
+
+# create a model object
+model = Model(inputs=vgg.input, outputs=prediction)
+
+# view the structure of the model
+model.summary()
+
+# tell the model what cost and optimization method to use
+model.compile(
+  loss='categorical_crossentropy',
+  optimizer='adam',
+  metrics=['accuracy']
+)
+
+
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+training_set = train_datagen.flow_from_directory('Datasets/Train',
+                                                 target_size = (224, 224),
+                                                 batch_size = 32,
+                                                 class_mode = 'categorical')
+
+test_set = test_datagen.flow_from_directory('Datasets/Test',
+                                            target_size = (224, 224),
+                                            batch_size = 32,
+                                            class_mode = 'categorical')
+
+'''r=model.fit_generator(training_set,
+                         samples_per_epoch = 8000,
+                         nb_epoch = 5,
+                         validation_data = test_set,
+                         nb_val_samples = 2000)'''
+
+# fit the model
+r = model.fit_generator(
+  training_set,
+  validation_data=test_set,
+  epochs=5,
+  steps_per_epoch=len(training_set),
+  validation_steps=len(test_set)
+)
+# loss
+plt.plot(r.history['loss'], label='train loss')
+plt.plot(r.history['val_loss'], label='val loss')
+plt.legend()
+plt.show()
+plt.savefig('LossVal_loss')
+
+# accuracies
+plt.plot(r.history['acc'], label='train acc')
+plt.plot(r.history['val_acc'], label='val acc')
+plt.legend()
+plt.show()
+plt.savefig('AccVal_acc')
+
+import tensorflow as tf
+
+from keras.models import load_model
+
+model.save('facefeatures_new_model.h5')
+
